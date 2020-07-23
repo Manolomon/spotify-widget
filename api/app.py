86f09a4de0bb29510e3c3f536d0da4c2aef320e5
Manolo Perez <manolope338@gmail.com>
2020-07-23 00:37:57 -0500

    Test new style

--- api/app.py
+++ api/app.py
@@ -16,7 +16,7 @@ SPOTIFY_REFRESH_TOKEN = os.getenv("SPOTIFY_REFRESH_TOKEN")
 # scope user-read-currently-playing/user-read-recently-played
 SPOTIFY_URL_REFRESH_TOKEN = "https://accounts.spotify.com/api/token"
 SPOTIFY_URL_NOW_PLAYING = "https://api.spotify.com/v1/me/player/currently-playing"
-SPOTIFY_URL_RECENTLY_PLAY = "https://api.spotify.com/v1/me/player/recently-played?limit=10"
+SPOTIFY_URL_RECENTLY_PLAY = "https://api.spotify.com/v1/me/player/recently-played?limit=1"
 
 
 app = Flask(__name__)
@@ -60,46 +60,32 @@ def nowPlaying():
 
     return response.json()
 
-def barGen(barCount):
-    barCSS = ""
-    left = 1
-    for i in range(1, barCount + 1):
-        anim = random.randint(1000, 1350)
-        barCSS += ".bar:nth-child({})  {{ left: {}px; animation-duration: {}ms; }}".format(
-            i, left, anim
-        )
-        left += 4
-
-    return barCSS
-
 def loadImageB64(url):
-    resposne = requests.get(url)
-    return b64encode(resposne.content).decode("ascii")
+    response = requests.get(url)
+    return b64encode(response.content).decode("ascii")
 
 def makeSVG(data):
-    barCount = 85
-    contentBar = "".join(["<div class='bar'></div>" for i in range(barCount)])
-    barCSS = barGen(barCount)
+
+    playing = True
 
     if data == {}:
-        content_bar = ""
         recent_plays = recentlyPlayed()
-        size_recent_play = len(recent_plays["items"])
-        idx = random.randint(0, size_recent_play - 1)
-        item = recent_plays["items"][idx]["track"]
+        item = recent_plays["items"][0]["track"]
+        playing = False
     else:
         item = data["item"]
 
     img = loadImageB64(item["album"]["images"][1]["url"])
     artistName = item["artists"][0]["name"].replace("&", "&amp;")
     songName = item["name"].replace("&", "&amp;")
+    albumName = item["album"]["name"].replace("&", "&amp;")
 
     dataDict = {
-        "content_bar": contentBar,
-        "css_bar": barCSS,
         "artist_name": artistName,
         "song_name": songName,
+        "album_name": albumName,
         "img": img,
+        "playing": playing
     }
 
     return render_template("spotify.html.j2", **dataDict)
